5bedc20017d5d80c4fb8af800c075e0ba611d7ce
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-20 22:24:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,6 +14,7 @@ def retrieval
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
 def adding
