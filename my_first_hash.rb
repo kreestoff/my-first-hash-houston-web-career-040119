c7246f9b94dd2262bb0f14fcdc2208ad11b926f9
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-20 22:23:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3}
 end
 
 def adding
@@ -23,5 +23,5 @@ def adding
                   "oil paintings" => 3
 shipping_manifest["muskets"] = 2 
 shipping_manifest["gun powder"] = 4
-shipping_manifest}
+shipping_manifest
 end
